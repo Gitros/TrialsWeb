415246fadf05962c235c176eb9ddb21986d70cee
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-07-01 18:52:16 +0200

    ProjectBuild

---
+++ TrialsWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrialsWeb.Models;
+
+namespace TrialsWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
